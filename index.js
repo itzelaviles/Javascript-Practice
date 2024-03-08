502026b6929618b31e682ae8b7853a951a759727
Itzel Aviles <itavgo8@gmail.com>
2024-03-08 14:51:39 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,58 @@
+//ASSIGNGING CONTENT TO HMTL ELEMENTS
+/*
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like videogames.`;
+*/
+
+
+//USING VARIABLES
+/*
+let age = 23;
+let name = 'Itzel';
+let favoriteFood = 'Pizza';
+
+console.log(typeof name);
+console.log(`I am ${age} years old.`);
+console.log(`My name is ${name}.`);
+console.log(`My favorite food is ${favoriteFood}.`);
+
+let isAdult = true;
+
+console.log(`Itzel is an adult: ${isAdult}.`)
+*/
+
+
+//USING VARIABLES AND DISPLAYING IN HTML.
+/*
+let fullName = 'Itzel Aviles';
+let age = 23;
+let isStudent = false;
+
+document.getElementById('p1').textContent = `My name is ${fullName}.`;
+document.getElementById('p2').textContent = `I am ${age} years old.`;
+document.getElementById('p3').textContent = `Enrolled: ${isStudent}.`;
+*/
+
+
+//ARITHMETIC OPERATORS
+/*
+let students = 30;
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+// students = students % 3;
+
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// students %= 2;
+//increment/decrement by 1
+// students++;
+// students--;
+console.log(students);
+*/
+
